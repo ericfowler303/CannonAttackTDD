5d34349674cb40b83e8c1facd3583fb270a95bef
Eric Fowler <ericfowler303@gmail.com>
2014-12-06 12:37:19 -0700

    Revert "Complete Iteration 2 of the tutorial"
    This reverts commit 3db36957f8b0d4ff92a3379951c7db0cefa08e42.

--- CannonAttack/CannonAttack/Cannon.cs
+++ CannonAttack/CannonAttack/Cannon.cs
@@ -7,8 +7,6 @@ namespace CannonAttack
     public sealed class Cannon
     {
         private readonly string CANNONID = "Human";
-        public static readonly int MAXANGLE = 90;
-        public static readonly int MINANGLE = 1;
         private string CannonID;
         public string ID
         {
@@ -37,15 +35,6 @@ namespace CannonAttack
                 }
                 return cannonSingletonInstance;
             }
-        }
-
-        public Tuple<bool, string> Shoot(int angle, int velocity)
-        {
-            if (angle > MAXANGLE || angle < MINANGLE) //Angle must be //between 0 and 90 degrees
-            {
-                return Tuple.Create(false, "Angle Incorrect");
-            }
-            return Tuple.Create(true, "Angle OK"); //Not //going to do the calculation just yet
         }
     }
 }
